435bcbb3b85f32d202c3fb8942e66674b9693895
robmllze <86869208+robmllze@users.noreply.github.com>
2024-03-21 12:53:51 +1100

    Format code

--- lib/src/_all_src.g.dart
+++ lib/src/_all_src.g.dart
@@ -7,15 +7,15 @@
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 //.title~
 
-export "annotations/field.dart";
-export "annotations/generate_makeups.dart";
-export "annotations/generate_model.dart";
-export "annotations/generate_screen_bindings.dart";
-export "annotations/generate_type_utils.dart";
-export "annotations/property.dart";
-export "annotations/test_annotation.dart";
-export "utils/generic_model.dart";
-export "utils/model.dart";
-export "utils/prep_value.dart";
-export "utils/this_model.dart";
-export "utils/types.dart";
+export 'annotations/field.dart';
+export 'annotations/generate_makeups.dart';
+export 'annotations/generate_model.dart';
+export 'annotations/generate_screen_bindings.dart';
+export 'annotations/generate_type_utils.dart';
+export 'annotations/property.dart';
+export 'annotations/test_annotation.dart';
+export 'utils/generic_model.dart';
+export 'utils/model.dart';
+export 'utils/prep_value.dart';
+export 'utils/this_model.dart';
+export 'utils/types.dart';
