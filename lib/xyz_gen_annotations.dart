c862143f04c7803b01e3e3aef23e96b8e6903cfb
robmllze <86869208+robmllze@users.noreply.github.com>
2024-05-06 00:31:29 +1000

    Update dependencies

--- lib/xyz_gen_annotations.dart
+++ lib/xyz_gen_annotations.dart
@@ -13,6 +13,8 @@ library;
 export 'dart:convert' show jsonEncode, jsonDecode;
 
 export 'package:equatable/equatable.dart';
+export 'package:meta/meta.dart';
+
 export 'package:xyz_utils/shared_src/web_friendly/_all_web_friendly.g.dart';
 
 export 'annotations_src/_all_annotations_src.g.dart';
